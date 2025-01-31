a2cb04908a58b1931005d8db8e88aedeb5eb620b
IfNoise <black_russan@mail.ru>
2025-01-31 11:30:07 +0700

    изменено:      src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,8 @@ async function bootstrap() {
   });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
   Logger.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
