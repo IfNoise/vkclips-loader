282ba92de3eab630f69655f996172c4a5c78531c
IfNoise <black_russan@mail.ru>
2025-01-31 11:54:13 +0700

    изменено:      src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     .setDescription('Short video VK publishing Restful API service')
     .setVersion('1.0')
     .addTag('vkpublish')
-    .setBasePath('swagger/')
+    .setBasePath('api/swagger/')
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document, {
